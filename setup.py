d54b8df7baece4800df9db83ee4b7fff143e4d33
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-10-11 17:14:34 +0300

    Revert "setup file"

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-   name='GraphSolver',
-   version='1.0',
-   description='Algebric equation solver displayed on graph.',
-   author='Ali, Ayham',
-   packages=['PyAlgebra']
-)
