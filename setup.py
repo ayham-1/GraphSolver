c27cdbb2a1fa6f4890773ca735269f6eaa71ec37
altffour <ayhamaboualfadl@gmail.com>
2019-10-12 13:00:28 +0300

    Add .swp files to .gitignore

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='GraphSolver',
+    version='0.0.1',
+    description='An application for solving linear\quadratic graphs.',
+    long_description=readme,
+    author='altf_four',
+    author_email='ayhamaboualfadl@gmail.com',
+    url='https://github.com/realaltffour/GraphSolver',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
