baf3c178a29f3c14756ab6a2c1f6d5109e3bc5b4
xypnox <xypnox@gmail.com>
2019-10-28 22:10:23 +0530

    Add calcLinearIntersection method
    This method calculates the intersection point between two Lines. This
    method returns the coordinates of intersection if there is a unique
    point, 0 if there are none, and math.inf if the lines coincide.

--- methods/linear/calcLinear.py
+++ methods/linear/calcLinear.py
@@ -1,3 +1,5 @@
+import math
+
 class Linear:
 
     def __init__(self, a, c, length):
@@ -7,3 +9,23 @@ class Linear:
 
     def calc_linear_line(self):
         print([(0 - self.length, self.a * (0 - self.length)), (0 - self.length, self.a * self.length)])
+
+    def calcLineaIntersection(self, line):
+        """
+        This function accepts one line of Linear class
+        and returns the coordinates of intersection between the self and the line passed
+
+        If there is no intercept it returns 0
+        If there are infinite intercepts it return math.inf
+        If there is one unique intercept it returns a tuple with (x, y)
+        """
+
+        if self.a == line.a:
+            if self.c == line.c:
+                return math.inf
+            return 0
+
+        x = (line.c-self.c)/(self.a - line.a)
+        y = self.a*(line.c - self.c)/(self.a - line.a) + self.c
+
+        return (x, y)
