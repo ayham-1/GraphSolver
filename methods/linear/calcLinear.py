e2c7677b25273cd55a2d310a7682719792300a01
Chae <delarosa.chae@gmail.com>
2019-10-27 21:39:51 -0400

    Add linearIntercept #20
    I think that's what you were asking for. If not i can change it.

--- methods/linear/calcLinear.py
+++ methods/linear/calcLinear.py
@@ -7,3 +7,11 @@ class Linear:
 
     def calc_linear_line(self):
         print([(0 - self.length, self.a * (0 - self.length)), (0 - self.length, self.a * self.length)])
+
+    def calc_intercept(self, x, y):
+
+    	y1 = (x + y) / 3
+    	y2 = ((3 * x) - (2 * y1) / 4
+
+    	print("Intersect at {}".format(y2))
+
