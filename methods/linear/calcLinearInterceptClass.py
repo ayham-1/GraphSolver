06974794cd92133878767d75603923f220b7ec36
TriptSharma <tript35.sharma@gmail.com>
2019-10-29 19:36:15 +0530

    Add calcLinearInterceptClass.py

---
+++ methods/linear/calcLinearInterceptClass.py
@@ -0,0 +1,27 @@
+import math
+
+class Linear:
+
+    def __init__(self, a, c):
+        # Line eq: y = ax + c
+        self.a = a
+        self.c = c
+
+    def calcLinearIntersection(self, line):
+        """
+        This function accepts one line of Linear class
+        and returns the coordinates of intersection between the self and the line passed
+        If there is no intercept it returns 0
+        If there are infinite intercepts it return math.inf
+        If there is one unique intercept it returns a tuple with (x, y)
+        """
+
+        if self.a == line.a:
+            if self.c == line.c:
+                return math.inf
+            return 0
+
+        x = (line.c-self.c)/(self.a - line.a)
+        y = self.a*(line.c - self.c)/(self.a - line.a) + self.c
+
+        return (x, y)
