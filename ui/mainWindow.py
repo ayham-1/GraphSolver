830394f161ecc49a9af454835fe56a744299a2f6
altffour <56314286+realaltffour@users.noreply.github.com>
2019-12-03 18:10:00 +0300

    Now button does something

--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtWidgets, uic
@@ -7,8 +7,11 @@ from PyQt5 import QtWidgets, uic
 class mainWin(QtWidgets.QMainWindow):
     def __init__(self):
         super(mainWin, self).__init__()
-        self.ui = uic.loadUi("ui/mainWindow.ui")
-
-        #self.runBtn = self.findChild(QWidget.QPushButton, "runBtn")
+        self.ui = uic.loadUi("ui/mainWindow.ui", self)
+        self.runBtn = self.findChild(QtWidgets.QPushButton, "runBtn")
+        self.runBtn.clicked.connect(self.runBtnPressed)
 
         self.ui.show()
+
+    def runBtnPressed(self):
+        print("Test")
